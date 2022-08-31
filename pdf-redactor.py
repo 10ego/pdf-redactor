762ccdaa085cd22b9b2c5def9485f14908717377
tkim <taehyun.kim@hc-sc.gc.ca>
2022-08-31 16:25:22 -0400

    fully featured

--- pdf-redactor.py
+++ pdf-redactor.py
@@ -1,6 +1,7 @@
 import fitz
 import re
-
+import json
+import hashlib
 class Redactor:
 # Fields to redact:
 #* A.2. Reporter Contact Information
@@ -35,10 +36,11 @@ class Redactor:
 	def redaction(self):
 		# opening the pdf
 		doc = fitz.open(self.path)
-		 
+		data = {}
 		# iterating through pages
 		for page in doc:
 			currentpage = 0 
+			
 			# _wrapContents is needed for fixing
 			# alignment issues with rect boxes in some
 			# cases where there is alignment issue
@@ -85,7 +87,6 @@ class Redactor:
 					XR0 = found[0][0]
 					B5_Y0 = found[0][3] + 2
 					XL1 = found[0][0] - 10
-#					XR1 = XR0 + XL1-XL0
 					XR1 = page.bound()[2]-XL0
 
 				Q_A7 = page.search_for('7. Name and address') # Reference for Reporter Contact Information Y1
@@ -104,7 +105,19 @@ class Redactor:
 				area_B5 = fitz.IRect(XR0, B5_Y0, XR1, YFOOTER)
 				areas_page1 = [area_A2, area_B4, area_B5]
 
-				[page.add_redact_annot(area, fill = (0, 0, 0)) for area in areas_page1]
+				text_A2 = page.get_text(clip=area_A2)
+				text_B4 = page.get_text(clip=area_B4)
+				text_B5 = page.get_text(clip=area_B5)
+				hash_A2 = hashlib.md5(text_A2.encode()).hexdigest()
+				hash_B4 = hashlib.md5(text_B4.encode()).hexdigest()
+				hash_B5 = hashlib.md5(text_B5.encode()).hexdigest()
+				data["A2"] = {"raw": text_A2, "hash":hash_A2}
+				data["B4"] = {"raw": text_B4, "hash":hash_B4}
+				data["B5"] = {"raw": text_B5, "hash":hash_B5}
+				
+				# [page.add_redact_annot(area, fill = (0, 0, 0)) for area in areas_page1]
+				[page.add_redact_annot(x[0], text=x[1]) for x in zip(areas_page1,[data[x]['hash'] for x in {k:v for k,v in data.items() if 'A' in k or 'B' in k}])] #this code is unreadable..should fix
+
 			elif currentpage == 2:
 				Q_D3 = page.search_for('3. Name of health care facility')
 				if Q_D3:
@@ -151,26 +164,51 @@ class Redactor:
 				area_E1 = fitz.IRect(XR0, E1_Y0, XR1, E1_Y1)
 				area_E2 = fitz.IRect(XR0, E2_Y0, XR1, E2_Y1)
 				area_E3 = fitz.IRect(XR0, E3_Y0, XR1, YFOOTER)
-#Run test to see if all areas are found:
-
 				areas_page2 = [
 					area_D2, area_D3, area_D4, area_E1, area_E2, area_E3
 				]
-		
-				[page.add_redact_annot(area, fill = (0, 0, 0)) for area in areas_page2]
+
+				text_D2 = page.get_text(clip=area_D2)
+				text_D3 = page.get_text(clip=area_D3)
+				text_D4 = page.get_text(clip=area_D4)
+				text_D5 = page.get_text(clip=area_D5)
+				text_E1 = page.get_text(clip=area_E1)
+				text_E2 = page.get_text(clip=area_E2)
+				text_E3 = page.get_text(clip=area_E3)
+				hash_D2 = hashlib.md5(text_D2.encode()).hexdigest()
+				hash_D3 = hashlib.md5(text_D3.encode()).hexdigest()
+				hash_D4 = hashlib.md5(text_D4.encode()).hexdigest()
+				hash_D5 = hashlib.md5(text_D5.encode()).hexdigest()
+				hash_E1 = hashlib.md5(text_E1.encode()).hexdigest()
+				hash_E2 = hashlib.md5(text_E2.encode()).hexdigest()
+				hash_E3 = hashlib.md5(text_E3.encode()).hexdigest()
+				data["D2"] = {"raw": text_D2, "hash":hash_D2}
+				data["D3"] = {"raw": text_D3, "hash":hash_D3}
+				data["D4"] = {"raw": text_D4, "hash":hash_D4}
+				data["D5"] = {"raw": text_D5, "hash":hash_D5}
+				data["E1"] = {"raw": text_E1, "hash":hash_E1}
+				data["E2"] = {"raw": text_E2, "hash":hash_E2}
+				data["E3"] = {"raw": text_E3, "hash":hash_E3}
+				
+
+				# [page.add_redact_annot(area, fill = (0, 0, 0)) for area in areas_page2]
+				[page.add_redact_annot(x[0], text=x[1]) for x in zip(areas_page2,[data[x]['hash'] for x in {k:v for k,v in data.items() if 'D' in k or 'E' in k}])] #this code is unreadable..should fix
 			# drawing outline over sensitive datas
 			# [page.add_redact_annot(area, text='REDACTED_DATA') for area in areas]
 			else:
 				page.add_redact_annot(page.bound())
+			
 			# applying the redaction
 			page.apply_redactions()
 		# saving it to a new pdf
-		doc.save('replaced5.pdf')
+		with open('pdf/'+self.path[:-4]+'.json', 'w+') as f:
+			json.dump(data, f)
+		doc.save('redacted/'+self.path[:-4]+'_REDACTED.pdf')
 		print("Successfully redacted")
 
 if __name__ == "__main__":
 	path = 'testing.pdf'
-	path = 'pdfs/000941728(F30)Attachment.pdf'
+	# path = 'pdfs/000941728(F30)Attachment.pdf'
 	
 	redactor = Redactor(path)
 	redactor.redaction() 
