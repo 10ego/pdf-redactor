527f419853df5c4e24cee51f13e23e3b56da7ed1
tkim <taehyun.kim@hc-sc.gc.ca>
2024-02-14 09:30:13 -0500

    final touches

--- pdf-redact-process-multithreaded.py
+++ pdf-redact-process-multithreaded.py
@@ -1,14 +1,14 @@
 import pdf_redactor3 as pdf_redactor
 from datetime import datetime
-from multiprocessing import Pool
+from multiprocessing import Pool, Value, Lock
 
 pdfs = []
 today = datetime.strftime(datetime.now(), '%Y-%m-%d_%H%M%S')
 datalist = 'pdfs.txt'
 worklog = 'worked_synthdata.log'
 errorlog = 'error_synthdata.log'
-INPUT_DIR = 'examples'
-OUTPUT_DIR = 'out'
+INPUT_DIR = 'pdfs'
+OUTPUT_DIR = 'synthetic'
 
 with open(datalist) as f:
     for line in f:
@@ -16,38 +16,38 @@ with open(datalist) as f:
 with open(worklog) as f:
     worked = [x[21:].strip() for x in f.readlines()]
 
-total = len(pdfs)
-count = 0
-POOL_SIZE = 5
+total = len(pdfs) - len(worked)
+counter = Value ('i', 0)
+lock = Lock()
 
 
-def pdfproc(pdf, worked=worked, count=0):
-    count += POOL_SIZE
+def pdfproc(pdf, worked=worked):
+    global counter, lock
 
-    if pdf not in worked:
-        print(f'{count}/{total}:', pdf)
-        try:
-            Redactor = pdf_redactor.Redactor(pdf, INPUT_DIR, OUTPUT_DIR)
-            Redactor.redaction()
-            with open(worklog, 'a+') as f:
+    with lock:
+        counter.value += 1
+        print(f"{counter}/{total}:", pdf)
+    try:
+        Redactor = pdf_redactor.Redactor(pdf, INPUT_DIR, OUTPUT_DIR)
+        Redactor.redaction()
+        with open(worklog, 'a+') as f:
+            f.write(
+                f'[{datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S")}]{pdf}\n'
+            )
+    except Exception as e:
+        print("Error. Skipping.")
+        with open(errorlog, 'a+') as f:
+            f.write(
+                f'[{datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S")}]{pdf}, {e.args}\n'
+            )
+        if not Redactor.lang:
+            with open('pertinence_synthdata.log', 'a+') as f:
                 f.write(
-                    f'[{datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S")}]{pdf}\n'
+                    f'[{datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S")}]{pdf}, IMPERTINENT\n'
                 )
-        except Exception as e:
-            print("Error. Skipping.")
-            with open(errorlog, 'a+') as f:
-                f.write(
-                    f'[{datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S")}]{pdf}, {e.args}\n'
-                )
-            if not Redactor.lang:
-                with open('pertinence_synthdata.log', 'a+') as f:
-                    f.write(
-                        f'[{datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S")}]{pdf}, IMPERTINENT\n'
-                    )
-    else:
-        pass
-
 
 if __name__ == '__main__':
+    POOL_SIZE = 5
+    INPUT_LIST = list(set(pdfs)-set(worked))
     with Pool(POOL_SIZE) as p:
-        p.map(pdfproc, pdfs)
+        p.map(pdfproc, INPUT_LIST)
