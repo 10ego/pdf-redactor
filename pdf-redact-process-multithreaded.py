80784da111a019d36219edc9c828c840fa928b57
Ted Kim <taehyun.kim@hc-sc.gc.ca>
2024-02-13 20:25:55 -0500

    fixing stuff

--- pdf-redact-process-multithreaded.py
+++ pdf-redact-process-multithreaded.py
@@ -1,5 +1,4 @@
 import pdf_redactor3 as pdf_redactor
-import os
 from datetime import datetime
 from multiprocessing import Pool
 
@@ -12,36 +11,43 @@ INPUT_DIR = 'emails/pdfs'
 OUTPUT_DIR = 'emails/redacted'
 
 with open(datalist) as f:
-	for line in f:
-		pdfs.append(line.rstrip())
+    for line in f:
+        pdfs.append(line.rstrip())
 with open(worklog) as f:
-	worked = [x[21:].strip() for x in f.readlines()]
+    worked = [x[21:].strip() for x in f.readlines()]
 
 total = len(pdfs)
-count=0
+count = 0
 POOL_SIZE = 5
-def pdfproc(pdf, worked = worked, count=0):
-	count+=POOL_SIZE
 
-	if not pdf in worked:
-		print(f'{count}/{total}:', pdf)
-		try:
-			Redactor = pdf_redactor.Redactor(pdf, INPUT_DIR, OUTPUT_DIR)
-			Redactor.redaction()
-			with open(worklog, 'a+') as f:
-				f.write(f'[{datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S")}]{pdf}\n')
-#		except Exception as e:
-		except Exception as e:
-			print("Error. Skipping.")
-			with open(errorlog, 'a+') as f:
-				f.write(f'[{datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S")}]{pdf}, {e.args}\n')
-			if not Redactor.lang:
-				with open('pertinence_synthdata.log', 'a+') as f:
-					f.write(f'[{datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S")}]{pdf}, IMPERTINENT\n')
-	else:
-		pass
-#		print(f'\t{pdf} already worked on')
 
-if __name__=='__main__':
-	with Pool(POOL_SIZE) as p:
-		p.map(pdfproc, pdfs)
+def pdfproc(pdf, worked=worked, count=0):
+    count += POOL_SIZE
+
+    if pdf not in worked:
+        print(f'{count}/{total}:', pdf)
+        try:
+            Redactor = pdf_redactor.Redactor(pdf, INPUT_DIR, OUTPUT_DIR)
+            Redactor.redaction()
+            with open(worklog, 'a+') as f:
+                f.write(
+                    f'[{datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S")}]{pdf}\n'
+                )
+        except Exception as e:
+            print("Error. Skipping.")
+            with open(errorlog, 'a+') as f:
+                f.write(
+                    f'[{datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S")}]{pdf}, {e.args}\n'
+                )
+            if not Redactor.lang:
+                with open('pertinence_synthdata.log', 'a+') as f:
+                    f.write(
+                        f'[{datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S")}]{pdf}, IMPERTINENT\n'
+                    )
+    else:
+        pass
+
+
+if __name__ == '__main__':
+    with Pool(POOL_SIZE) as p:
+        p.map(pdfproc, pdfs)
